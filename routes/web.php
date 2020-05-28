2f8922dd4547b10220c35079a798c7c30e92420c
minhtn143 <minhtn143@gmail.com>
2020-05-28 09:25:11 +0700

    CRUD Customers Table

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,16 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+//tao group route customers
+Route::group(['prefix' => 'customers'], function () {
+    Route::get('/','CustomerController@index')->name('customers.index');
+    Route::get('/create','CustomerController@create')->name('customers.create');
+    Route::post('/create','CustomerController@store')->name('customers.store');
+    Route::get('/{id}/edit','CustomerController@edit')->name('customers.edit');
+    Route::post('/{id}/edit','CustomerController@update')->name('customers.update');
+    Route::get('/{id}/destroy','CustomerController@destroy')->name('customers.destroy');
 });
